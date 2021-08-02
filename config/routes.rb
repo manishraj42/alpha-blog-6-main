1cbbada147d553625a5830f8f1d72e8d05fda478
manishraj42 <manish.raj@anchanto.com>
2021-08-02 23:26:20 +0530

    add articles listing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' , to:'pages#about'
   #the below line imports the articles model to be used for backend
-  resources :articles, only: [:show]
+  resources :articles #, only: [:show]
 end

6fa4517f70ded265cfe7ccb0f151b2aeadd66fe0
manishraj42 <manish.raj@anchanto.com>
2021-08-02 23:11:07 +0530

    database and models added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' , to:'pages#about'
+  #the below line imports the articles model to be used for backend
+  resources :articles, only: [:show]
 end

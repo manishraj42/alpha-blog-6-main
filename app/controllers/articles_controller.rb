2d5648355878421f474a7b17350f023f96be0bdf
manishraj42 <manish.raj@anchanto.com>
2021-08-03 13:53:45 +0530

    added edit route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,33 @@ class ArticlesController < ApplicationController
     
     end
     def new 
+        @article=Article.new
 
+    end
+    def edit 
+        @article=Article.find(params[:id])
     end
     def create 
         @article = Article.new(params.require(:article).permit(:title,:description))
-        @article.save
-        redirect_to article_path(@article)
+        if @article.save
+            #to flash the message
+            flash[:notice] = "Article was created successfully!"
+            redirect_to article_path(@article)
+        else 
+            render 'new'
+
+        end
+    end
+    def update 
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] = "Article was updated successfully!S"
+            redirect_to @article 
+        else 
+            render 'edit'
+
+        end
+
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' , to:'pages#about'
   #the below line imports the articles model to be used for backend
-  resources :articles , only: [:show, :new,:create,:index]
+  resources :articles , only: [:show, :new,:create,:index, :edit, :update]
 end

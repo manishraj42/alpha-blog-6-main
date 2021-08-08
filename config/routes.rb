708e2a359dbfbae202e1b9bd13d10521b0cb2e86
manishraj42 <manish.raj@anchanto.com>
2021-08-08 13:34:06 +0530

    signup form added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about' , to:'pages#about'
   #the below line imports the articles model to be used for backend
   resources :articles , only: [:show, :new,:create,:index, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users , except: [:new]
 end

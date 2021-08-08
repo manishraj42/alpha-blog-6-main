43030ba5d620160973eadd9647e51b2c1fcbdedf
manishraj42 <manish.raj@anchanto.com>
2021-08-08 11:44:08 +0530

    add one-to-many users and articles from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
     end
     def create 
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             #to flash the message
             flash[:notice] = "Article was created successfully!"

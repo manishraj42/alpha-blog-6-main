a25f4bacfb2d98cc897d657e9776efa6ac3a8d29
manishraj42 <manish.raj@anchanto.com>
2021-08-12 00:49:45 +0530

    profile page added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,59 +1,56 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only:[:show, :edit, :update, :destroy]
-    def show 
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
 
-        # byebug
-    end
+	def show 
+		# byebug
+	end
 
-    def index 
-        @articles = Article.all
-    
-    end
-    def new 
-        @article=Article.new
+	def index 
+		@articles = Article.all
+	end
 
-    end
-    def edit 
-    end
-    def create 
-        @article = Article.new(article_params)
-        @article.user = User.first
-        if @article.save
-            #to flash the message
-            flash[:notice] = "Article was created successfully!"
-            redirect_to article_path(@article)
-        else 
-            render 'new'
+	def new 
+		@article=Article.new
+	end
 
-        end
-    end
-    def update 
-        if @article.update(article_params)
-            flash[:notice] = "Article was updated successfully!"
-            redirect_to @article 
-        else 
-            render 'edit'
+	def edit 
+	end
 
-        end
+	def create 
+		@article = Article.new(article_params)
+		@article.user = User.first
+		if @article.save
+			#to flash the message
+			flash[:notice] = "Article was created successfully!"
+			redirect_to article_path(@article)
+		else 
+			render 'new'
+		end
+	end
 
+	def update 
+		if @article.update(article_params)
+			flash[:notice] = "Article was updated successfully!"
+			redirect_to @article 
+		else 
+			render 'edit'
+		end
+	end
 
-    end
-    def destroy 
-        @article.destroy 
-        redirect_to articles_path
+	def destroy 
+		@article.destroy 
+		redirect_to articles_path
+	end
 
-    end
+	private 
 
-    private 
+	def set_article
+		@article = Article.find(params[:id])
+	end
 
-    def set_article
-        @article = Article.find(params[:id])
-    end
+	def article_params
+		params.require(:article).permit(:title,:description)
+	end
 
-    def article_params
-        params.require(:article).permit(:title,:description)
-    end
-
-    #don't put an end for the private
-
+	#don't put an end for the private
 end

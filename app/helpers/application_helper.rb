20049dba7b08b30dd0a7b058e017677afa2d38a5
manishraj42 <manish.raj@anchanto.com>
2021-08-13 00:48:46 +0530

    user_controller :: admin added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,16 @@ module ApplicationHelper
 		image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
 	end
 
+	def flash_class(level)
+		bootstrap_alert_class = {
+			"success" => "alert-success",
+			"error" => "alert-danger",
+			"notice" => "alert-info",
+			"alert" => "alert-danger",
+			"warn" => "alert-warning"
+		}
+		bootstrap_alert_class[level]
+	end
+
 	
 end
